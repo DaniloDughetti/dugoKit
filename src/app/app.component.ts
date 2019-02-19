7e15a6eb0f1108bd231e195f8521a87734ade73a
Danilo Dughetti <danilo.dughetti@gmail.com>
2019-02-20 00:25:10 +0100

    import bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,27 @@ export class AppComponent {
     // carouselItem.type = Constants.carouselItemTypeFromApi;
     this.userItems.push({
       id: 0,
+      name: 'User 0',
+      type: 0
+    },{
+      id: 1,
       name: 'User 1',
+      type: 0
+    },{
+      id: 2,
+      name: 'User 2',
+      type: 0
+    },{
+      id: 3,
+      name: 'User 3',
+      type: 0
+    },{
+      id: 4,
+      name: 'User 4',
+      type: 0
+    },{
+      id: 5,
+      name: 'User 5',
       type: 0
     });
   }
